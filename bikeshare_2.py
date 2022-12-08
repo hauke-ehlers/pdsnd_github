b290d8b331dbe9141c7fa1e1c7374f1a70b71a92
Hauke Ehlers <hauke.ehlers@vodafone.com>
2022-12-08 17:04:35 +0100

    docs: Add comment to bikeshare_2.py
    Added comment in line 20 to simulate documentation change.

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -17,7 +17,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
+	# added this comment to simulate documentation change.
 
     # get user input for month (all, january, february, ... , june)
 
